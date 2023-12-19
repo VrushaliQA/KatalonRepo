92abb7604ea78143b5fda30b412e551ee7e35b0d
Vrushali <testingpurposesel@gmail.com>
2023-12-19 13:31:19 +0530

    added Jira TestCase for share case

--- Test Suites/SanitySuite.ts
+++ Test Suites/SanitySuite.ts
@@ -14,15 +14,29 @@
    <testCaseLink>
       <guid>23d36ca5-a48e-4201-aafd-3dcb2d50cad1</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/LoginTest</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>fe13fffc-6900-4c1f-8ddb-ecb70cdd0008</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/ShareCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc819b82-47c2-4281-a0e1-f82cf58b0439</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SCRUM-1 User login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>279966e8-7cf3-4e69-962f-a33eb30585ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/SCRUM-2 share the case with user</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
