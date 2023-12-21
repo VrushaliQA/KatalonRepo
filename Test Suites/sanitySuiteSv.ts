cd03f95ee5ef7d90cd4a7edd8dd91d66cd2b56a2
Vrushali <testingpurposesel@gmail.com>
2023-12-21 13:05:46 +0530

    AddedSVtestCases

---
+++ Test Suites/sanitySuiteSv.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>sanitySuiteSv</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e808dd8c-6117-4ea4-b551-68858582c73d</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c663038-5c4d-43ee-8472-75a6f9b19c32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/sv/CreateCircleOnSV</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
